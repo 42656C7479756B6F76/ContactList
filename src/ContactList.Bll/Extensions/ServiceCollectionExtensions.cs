1f1981fa5f8d78dc48291841eba1cb51f4bbde18
Beltyukov Danil <beltyukov.danil.evgenevich@gmail.com>
2024-12-13 08:08:22 +0300

    Register bll services

---
+++ src/ContactList.Bll/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using ContactList.Bll.Models.Contact;
+using ContactList.Bll.Services;
+using ContactList.Bll.Services.Interfaces;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ContactList.Bll.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddBllServices(
+        this IServiceCollection services)
+    {
+        services.AddScoped<IPersonService, PersonService>();
+        services.AddScoped<IContactService, ContactService>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddBllValidators(
+        this IServiceCollection services)
+    {
+        return services
+            .AddValidatorsFromAssemblyContaining<AddContactModel>();
+    }
+}
