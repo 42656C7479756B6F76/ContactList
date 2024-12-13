f0aba2c130bbcb4736f272d6ebefb8f7d6dbccdf
Beltyukov Danil <beltyukov.danil.evgenevich@gmail.com>
2024-12-13 08:09:33 +0300

    Using all layers

--- src/ContactList.Api/Program.cs
+++ src/ContactList.Api/Program.cs
@@ -1,21 +1,35 @@
+using ContactList.Api.Middlewares;
+using ContactList.Bll.Extensions;
+using ContactList.Dal.Extensions;
+using FluentValidation.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+var services = builder.Services;
+
+services
+    .AddDalInfrastructure(builder.Configuration)
+    .AddDalRepositories()
+    .AddBllServices();
+
+services
+    .AddBllValidators()
+    .AddFluentValidationAutoValidation();
+
+services.AddControllers();
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
 app.MapControllers();
+
+app.UseMiddleware<ErrorMiddleware>();
+
+app.MigrateUp();
 
 app.Run();
