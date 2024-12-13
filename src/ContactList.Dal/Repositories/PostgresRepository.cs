a148c0e80c2a0f1186726d9dc7c15b1c28558e0b
Beltyukov Danil <beltyukov.danil.evgenevich@gmail.com>
2024-12-13 07:59:07 +0300

    Added implementations of repositories

---
+++ src/ContactList.Dal/Repositories/PostgresRepository.cs
@@ -0,0 +1,32 @@
+﻿using System.Transactions;
+using ContactList.Dal.Repositories.Interfaces;
+using ContactList.Dal.Settings;
+using Npgsql;
+
+namespace ContactList.Dal.Repositories;
+
+public abstract class PostgresRepository : IPostgresRepository
+{
+    protected const int DefaultTimeoutInSeconds = 5;
+    private readonly DalOptions _dalSettings;
+
+    protected PostgresRepository(DalOptions dalSettings)
+    {
+        _dalSettings = dalSettings;
+    }
+
+    protected async Task<NpgsqlConnection> GetConnection()
+    {
+        if (Transaction.Current is not null &&
+            Transaction.Current.TransactionInformation.Status is TransactionStatus.Aborted)
+            throw new TransactionAbortedException("Transaction was aborted (probably by user cancellation request)");
+
+        var connection = new NpgsqlConnection(_dalSettings.PostgresConnectionString);
+        await connection.OpenAsync();
+
+        // Due to in-process migrations
+        connection.ReloadTypes();
+
+        return connection;
+    }
+}
