2e2997a17d13d1997a7e54927f0a9699d7941fca
Beltyukov Danil <beltyukov.danil.evgenevich@gmail.com>
2024-12-13 08:04:14 +0300

    Validators for services argument

---
+++ src/ContactList.Bll/Validators/Person/UpdatePersonModelValidator.cs
@@ -0,0 +1,20 @@
+﻿using ContactList.Bll.Models.Person;
+using FluentValidation;
+
+namespace ContactList.Bll.Validators.Person;
+
+public class UpdatePersonModelValidator : AbstractValidator<UpdatePersonModel>
+{
+    public UpdatePersonModelValidator()
+    {
+        RuleFor(request => request.Id)
+            .Must(x => x > 0)
+            .WithMessage("Id must be positive.");
+        RuleFor(request => request.FirstName)
+            .NotEmpty()
+            .WithMessage("FirstName must be not empty.");
+        RuleFor(request => request.LastName)
+            .NotEmpty()
+            .WithMessage("LastName must be not empty.");
+    }
+}
