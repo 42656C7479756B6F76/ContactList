d7344b5cc3cef71203f73ed483b43eef3e37c940
Beltyukov Danil <beltyukov.danil.evgenevich@gmail.com>
2024-12-13 07:54:24 +0300

    Interfaces of repositories

---
+++ src/ContactList.Dal/Repositories/Interfaces/IContactRepository.cs
@@ -0,0 +1,16 @@
+﻿using ContactList.Dal.Models.Contact;
+using ContactList.Dal.Models.Person;
+using ContactList.Domain;
+
+namespace ContactList.Dal.Repositories.Interfaces;
+
+public interface IContactRepository
+{
+    Task<long[]> Add(ContactEntityV1[] contacts, CancellationToken token);
+
+    Task<ContactEntityV1[]> GetPersonContacts(PersonGetContactsModel query, CancellationToken token);
+
+    Task<ContactEntityV1> Update(ContactUpdateModel query, CancellationToken token);
+
+    Task<bool> Delete(ContactDeleteModel query, CancellationToken token);
+}
---
+++ src/ContactList.Dal/Repositories/Interfaces/IPersonRepository.cs
@@ -0,0 +1,15 @@
+﻿using ContactList.Dal.Models.Person;
+using ContactList.Domain;
+
+namespace ContactList.Dal.Repositories.Interfaces;
+
+public interface IPersonRepository
+{
+    Task<long[]> Add(PersonEntityV1[] persons, CancellationToken token);
+
+    Task<PersonEntityV1> Get(PersonGetModel query, CancellationToken token);
+
+    Task<PersonEntityV1> Update(PersonUpdateModel query, CancellationToken token);
+
+    Task<bool> Delete(PersonDeleteModel query, CancellationToken token);
+}
